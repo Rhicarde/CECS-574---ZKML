ba874e38c3084dc6755c83ae5a20cadefbdaaabe
Richard Huynh <richardsang2001@gmail.com>
2025-03-02 02:55:38 -0800

    Committing initial changes - Downloading and viewing dataset that will be used

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import kagglehub
+import numpy as np
+import pandas as pd
+
+def download_dataset():
+    # Downloading Dataset
+    path = kagglehub.dataset_download("ankushpanday2/heart-attack-risk-and-prediction-dataset-in-india")
+    print("Path to dataset files:", path)
+
+    return path
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print('Hello World')
+    path = download_dataset() + '\\heart_attack_prediction_india.csv'
+    df = pd.read_csv(path)
+
+    print(df.head())
+    print(df.describe())
+    print(df.columns)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/

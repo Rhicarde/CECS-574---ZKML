768470e07af77db00b54f6e6b74b680f17f2c40a
Richard Huynh <richardsang2001@gmail.com>
2025-03-02 21:35:41 -0800

    Committing initial changes - Downloading and viewing dataset that will be used

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import kagglehub
 import numpy as np
 import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler , OneHotEncoder
+from sklearn.metrics import accuracy_score, confusion_matrix
+import warnings
+warnings.filterwarnings('ignore')
 
 def download_dataset():
     # Downloading Dataset
@@ -9,13 +16,37 @@ def download_dataset():
 
     return path
 
+def train_test(df, epoch=20):
+    x = df.drop(columns=['Heart_Attack_Risk', 'Patient_ID'])
+    y = df['Heart_Attack_Risk']
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=2)
+
+    ct = ColumnTransformer(transformers=
+    [
+        ('onehot', OneHotEncoder(drop='first'), ['State_Name', 'Gender']),
+        ('normal', StandardScaler(),
+         ['Diastolic_BP', 'Annual_Income', 'Emergency_Response_Time', 'Systolic_BP', 'Cholesterol_Level',
+          'Triglyceride_Level', 'LDL_Level', 'HDL_Level'])
+    ], remainder='passthrough'
+    )
+
+    x_train_ct = ct.fit_transform(x_train)
+    x_test_ct = ct.transform(x_test)
+
+    model = LogisticRegression()
+    model.fit(x_train_ct, y_train)
+    ypred = model.predict(x_test_ct)
+
+    print('accuracy_score', accuracy_score(y_test, ypred))
+    print('Confusion_matrix', confusion_matrix(y_test, ypred))
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     path = download_dataset() + '\\heart_attack_prediction_india.csv'
     df = pd.read_csv(path)
 
-    print(df.head())
-    print(df.describe())
-    print(df.columns)
+    train_test(df)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
